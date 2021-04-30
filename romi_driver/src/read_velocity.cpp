39f6baf7073de9737ad68ceb9fec6ea1835bf37f
Rodrigo Causarano <rjcausarano@gmail.com>
2021-04-30 11:58:56 -0400

    creates motors_node

--- romi_driver/src/read_velocity.cpp
+++ romi_driver/src/read_velocity.cpp
@@ -12,11 +12,11 @@ int get_rpm(){
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "encoders_node");
 
   ros::NodeHandle n;
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("/counts", 1000);
 
   ros::Rate loop_rate(10);
 
--- romi_driver/src/write_velocity.cpp
+++ romi_driver/src/write_velocity.cpp
@@ -1,16 +1,19 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include <iostream>
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void commandVelCB(const geometry_msgs::Twist::ConstPtr& msg)
 {
-ROS_INFO("I heard: [%s]", msg->data.c_str());
+  std::cout << "linear x: " << msg->linear.x << std::endl;
+  std::cout << "linear y: " << msg->linear.y << std::endl;
+  std::cout << "linear z: " << msg->linear.z << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "motors_node");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, commandVelCB);
 
   ros::spin();
 
