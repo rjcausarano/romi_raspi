4bdf1cfc0c229db85af1dc1caa190261b94d62e6
Rodrigo Causarano <rjcausarano@gmail.com>
2021-04-30 15:32:40 -0400

    i2ctest toggles motor

--- cpp_test/i2ctest.cpp
+++ cpp_test/i2ctest.cpp
@@ -9,18 +9,18 @@ int main()
    int fd, result, offset;
 
    fd = wiringPiI2CSetup(0x4);
-   offset = 5;
+   offset = 0;
 
    cout << "I2C setup successful: "<< fd << endl;
 
    result = wiringPiI2CReadReg8(fd, offset);
 
-   cout << "LED status: " << result << std::endl;
-
-   // toggle LED
+   // toggle Motor
    int status = 0;
    while(true){
       wiringPiI2CWriteReg8(fd, offset, status);
+      cout << "Motor status: " << status << std::endl;
+
       status = !status;
       sleep(5);
    }
