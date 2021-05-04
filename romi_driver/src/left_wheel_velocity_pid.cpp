d918123ac0777eb699d58726ce1a1cbb3ebb4aa9
Rodrigo Causarano <rjcausarano@gmail.com>
2021-05-04 18:03:05 -0400

    proportional controller

--- romi_driver/src/left_wheel_velocity_pid.cpp
+++ romi_driver/src/left_wheel_velocity_pid.cpp
@@ -1,12 +1,28 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 #include <iostream>
+#include <wiringPiI2C.h>
 
 float desired_ang_vel_ = 0;
+int P_ = 100, I_ = 1, D_ = 1, fd_ = 0, pwm_pid_offset_ = 6;
 
-void commandVelCB(const std_msgs::Float32::ConstPtr& msg)
+void do_pid(float current, float desired){
+  int error = desired - current;
+  int p_error = P_ * error;
+  int correction = p_error;
+  if(correction < -1023){
+    correction = -1023;
+  } else if(correction > 1023){
+    correction = 1023;
+  }
+  wiringPiI2CWriteReg16(fd_, pwm_pid_offset_, correction);
+}
+
+void currentVelCB(const std_msgs::Float32::ConstPtr& msg)
 {
-  std::cout << "Angular velocity: " << msg->data << " Rad/s" << std::endl;
+  float current_ang_vel = msg->data;
+  std::cout << "Angular velocity: " << current_ang_vel << " Rad/s" << std::endl;
+  do_pid(current_ang_vel, desired_ang_vel_);
 }
 
 void pidVelCB(const std_msgs::Float32::ConstPtr& msg){
@@ -15,9 +31,10 @@ void pidVelCB(const std_msgs::Float32::ConstPtr& msg){
 
 int main(int argc, char **argv)
 {
+  fd_ = wiringPiI2CSetup(0x4);
   ros::init(argc, argv, "pid_node");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/left_wheel_vel", 1000, commandVelCB);
+  ros::Subscriber sub = n.subscribe("/left_wheel_vel", 1000, currentVelCB);
   ros::Subscriber pid_sub = n.subscribe("/left_wheel_pid", 1000, pidVelCB);
   ros::spin();
 
