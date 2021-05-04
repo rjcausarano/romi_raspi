e488fa3e427b21dbd8bcff56c4d89900906da9b5
Rodrigo Causarano <rjcausarano@gmail.com>
2021-05-04 12:05:51 -0400

    set direction to forward before sending pwm speed

--- cpp_test/send_pwm_percent.cpp
+++ cpp_test/send_pwm_percent.cpp
@@ -4,23 +4,28 @@
 
 int main()
 {
-   int fd = 0, pwm_percent = 0, pwm_offset = 0, motor_offset = 0;
+   int fd = 0, pwm_percent = 0, pwm_offset = 0, motor_offset = 0, wheel_dir_offset = 0;;
 
    fd = wiringPiI2CSetup(0x4);
    // PWM offset is 1, motor offset is 0
-   pwm_offset = 1;
    motor_offset = 0;
+   pwm_offset = 1;
+   wheel_dir_offset = 2;
 
    std::cout << "I2C setup successful"<< fd << std::endl;
-
-   // Enable motor, 1 to enable 0 to disable
-   wiringPiI2CWriteReg8(fd, motor_offset, 1);
+   wiringPiI2CWriteReg8(fd, wheel_dir_offset, 1);
 
    while(true){
       std::cout << "PWM percent: " << std::endl;
       std::cin >> pwm_percent;
       if(pwm_percent >= 0 && pwm_percent <= 100){
          std::cout << "sending speed command!" << std::endl;
+         if(pwm_percent > 0){
+            // Enable motor, 1 to enable 0 to disable
+            wiringPiI2CWriteReg8(fd, motor_offset, 1);
+         } else{
+            wiringPiI2CWriteReg8(fd, motor_offset, 0);
+         }
          wiringPiI2CWriteReg8(fd, pwm_offset, pwm_percent);
       } else {
          std::cout << "number not in range 0-100" << std::endl;
