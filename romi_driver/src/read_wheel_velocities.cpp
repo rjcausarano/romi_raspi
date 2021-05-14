8fbc71db33f16816ea1382ef1efdf76aa5273d52
Rodrigo Causarano <rjcausarano@gmail.com>
2021-05-14 16:57:17 -0400

    adds camera streaming

--- romi_driver/src/read_wheel_velocities.cpp
+++ romi_driver/src/read_wheel_velocities.cpp
@@ -5,19 +5,27 @@
 #include <chrono>
 
 // right wheel: 3 left wheel: 4
-int left_device = 0, right_device = 0, address = 3, dir_offset = 2, vel_offset = 3;
-int pub_rate = 10, counts_per_rev = 1440;
-std::chrono::steady_clock::time_point last_time = std::chrono::steady_clock::now();
+int left_device_ = 0, right_device_ = 0, address_ = 3, dir_offset_ = 2, vel_offset_ = 3;
+int pub_rate_ = 10, counts_per_rev_ = 1440;
+std::chrono::steady_clock::time_point llast_time_ = std::chrono::steady_clock::now();
+std::chrono::steady_clock::time_point rlast_time_ = std::chrono::steady_clock::now();
 
-float get_angular_vel(int device){
+float get_left_angular_vel(){
   std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
-  float time_elapsed = std::chrono::duration<float>(now - last_time).count();
-  last_time = now;
-  return ((float) wiringPiI2CReadReg16(device, vel_offset) / 4) / time_elapsed;
+  float time_elapsed = std::chrono::duration<float>(now - llast_time_).count();
+  llast_time_ = now;
+  return ((float) wiringPiI2CReadReg16(left_device_, vel_offset_) / 4) / time_elapsed;
+}
+
+float get_right_angular_vel(){
+  std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+  float time_elapsed = std::chrono::duration<float>(now - rlast_time_).count();
+  rlast_time_ = now;
+  return ((float) wiringPiI2CReadReg16(right_device_, vel_offset_) / 4) / time_elapsed;
 }
 
 int get_wheel_direction(int device){
-  return wiringPiI2CReadReg8(device, dir_offset);
+  return wiringPiI2CReadReg8(device, dir_offset_);
 }
 
 float degrees_to_radians(float deg){
@@ -35,20 +43,20 @@ int main(int argc, char **argv)
   ros::Publisher left_vel_pub = n.advertise<std_msgs::Float32>("/left_wheel_vel", 1000);
   ros::Publisher right_vel_pub = n.advertise<std_msgs::Float32>("/right_wheel_vel", 1000);
 
-  ros::Rate loop_rate(pub_rate);
+  ros::Rate loop_rate(pub_rate_);
 
-  right_device = wiringPiI2CSetup(address);
-  left_device = wiringPiI2CSetup(address + 1);
+  right_device_ = wiringPiI2CSetup(address_);
+  left_device_ = wiringPiI2CSetup(address_ + 1);
 
   while (ros::ok())
   {
     std_msgs::Float32 lmsg;
     std_msgs::Float32 rmsg;
 
-    float lrad_ps = degrees_to_radians(get_angular_vel(left_device));
+    float lrad_ps = degrees_to_radians(get_left_angular_vel());
     lmsg.data = lrad_ps;
 
-    float rrad_ps = degrees_to_radians(get_angular_vel(right_device));
+    float rrad_ps = degrees_to_radians(get_right_angular_vel());
     rmsg.data = rrad_ps;
 
     std::cout << "left rad/s: " << lrad_ps << std::endl;
